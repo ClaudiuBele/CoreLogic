dd514205f5c3856d5c4051f8014b6064042cffe2
Claudiu Bele <claudiu.bele.dev@gmail.com>
2020-10-25 12:32:36 +0100

    Get white or black color from background

--- corelogic/src/main/java/dk/sidereal/corelogic/platform/util/ColorUtils.kt
+++ corelogic/src/main/java/dk/sidereal/corelogic/platform/util/ColorUtils.kt
@@ -5,9 +5,9 @@ import androidx.annotation.ColorInt
 
 object ColorUtils {
 
-    class ColorData{
+    class ColorData {
 
-        constructor(r:Float,g: Float, b: Float, a: Float ) {
+        constructor(r: Float, g: Float, b: Float, a: Float) {
             this.r = r;
             this.g = g;
             this.b = b;
@@ -45,6 +45,24 @@ object ColorUtils {
         val b = (color and 0xff) / 255.0f
         val a = (color shr 24 and 0xff) / 255.0f
         return ColorData(r, g, b, a)
+    }
+
+    fun isContrastColorWhite(@ColorInt color: Int): Boolean {
+        // Counting the perceptive luminance - human eye favors green color...
+        val a =
+            1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255
+        val d: Int
+        return a >= 0.5
+    }
+
+    @ColorInt
+    fun getContrastColor(@ColorInt color: Int): Int {
+        val d = if (!isContrastColorWhite(color)) {
+            0 // bright colors - black font
+        } else {
+            255 // dark colors - white font
+        }
+        return Color.rgb(d, d, d)
     }
 
     @ColorInt
